e1259b99863301a7835676493ff611ac80a43591
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2026-01-20 18:30:45 +0100

    feat: add validation for Entity ID in constructor and implement equals method

--- src/shared/domain/Entity.ts
+++ src/shared/domain/Entity.ts
@@ -6,11 +6,28 @@ export abstract class Entity<T> {
   protected readonly _id: T;
   // 2. Constructor que recibe el ID
   constructor(id: T) {
+    if (id === null || id === undefined) {
+      throw new Error('Entity ID cannot be null or undefined');
+    }
     this._id = id;
   }
 
   // 3. Getter público para el ID
   get id(): T {
     return this._id;
+  }
+
+  // 4. Método equals para comparar entidades por su ID
+  public equals(object?: Entity<T>): boolean {
+    if (object === null || object === undefined) {
+      return false;
+    }
+    if (this === object) {
+      return true;
+    }
+    if (this.constructor !== object.constructor) {
+      return false;
+    }
+    return this._id === object._id;
   }
 }
